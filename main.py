d5647ed6e41e145ee591b3bec614ed60d6e3cd22
HuWeilin <kevislin@HuWeilindeMacBook-Air.local>
2022-03-15 20:13:58 +0800

    代码修改

--- main.py
+++ main.py
@@ -252,37 +252,19 @@ SMPath = {
 }
 
 config = {
-    'epoch_GCN':100, # Huang model 训练的epoch
-    'epoch_CPM':1000,
-    'lsd_dim':64, # CPM_net latent space dimension
+    'epoch_GCN':1000, # Huang model 训练的epoch
+    'epoch_CPM':5000,
+    'lsd_dim':128, # CPM_net latent space dimension
     'CPM_lr':[0.0005, 0.0005], # CPM_ner中train和test的学习率
     'ref_class_num':9, # Reference data的类别数
     'query_class_num':9, # query data的类别数
     'k':4, # 图构造的时候k_neighbor参数
-    'middle_out':128 # GCN中间层维数
+    'middle_out':256 # GCN中间层维数
 }
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-print("64---32")
-config['middle_out'] = 64
-config['lsd_dim'] = 32
-transfer_labels(dataPath, labelPath, SMPath, config)
 
-print("128---64")
-config['middle_out'] = 128
-config['lsd_dim'] = 64
-transfer_labels(dataPath, labelPath, SMPath, config)
-
-
-print("256---128")
-config['middle_out'] = 256
-config['lsd_dim'] = 128
-transfer_labels(dataPath, labelPath, SMPath, config)
-
-print("512---256")
-config['middle_out'] = 512
-config['lsd_dim'] = 256
 transfer_labels(dataPath, labelPath, SMPath, config)
 
 # 最后进行一个分类
