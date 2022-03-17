cebdecef238af501217c369876057bd9cbc534ef
HuWeilin <kevislin@HuWeilindeMacBook-Air.local>
2022-03-17 15:14:39 +0800

    显示representation的distribution

--- main.py
+++ main.py
@@ -258,13 +258,13 @@ SMPath = {
 }
 
 config = {
-    'epoch_GCN':3000, # Huang model 训练的epoch
-    'epoch_CPM':5000,
+    'epoch_GCN':100, # Huang model 训练的epoch
+    'epoch_CPM':1000,
     'lsd_dim':128, # CPM_net latent space dimension
     'CPM_lr':[0.001, 0.001], # CPM_ner中train和test的学习率
     'ref_class_num':9, # Reference data的类别数
     'query_class_num':9, # query data的类别数
-    'k':10, # 图构造的时候k_neighbor参数
+    'k':4, # 图构造的时候k_neighbor参数
     'middle_out':256,  # GCN中间层维数
     'w_classify': 1 # classfication loss的权重
 }
