65661e399d87addebe4e12d73f69193e038f727f
HuWeilin <kevislin@HuWeilindeMacBook-Air.local>
2022-03-19 16:40:12 +0800

    代码调整

--- main.py
+++ main.py
@@ -226,9 +226,9 @@ SMPath = {
 
 config = {
     'epoch_GCN': 3000,  # Huang model 训练的epoch
-    'epoch_CPM': 5000,
+    'epoch_CPM': 3000,
     'lsd_dim': 128,  # CPM_net latent space dimension
-    'CPM_lr': [0.005, 0.005],  # CPM_ner中train和test的学习率
+    'CPM_lr': [0.05, 0.05],  # CPM_ner中train和test的学习率
     'ref_class_num': 9,  # Reference data的类别数
     'query_class_num': 9,  # query data的类别数
     'k': 4,  # 图构造的时候k_neighbor参数
