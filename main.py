332618662a94a6305aadaaacb5a679787ca88c1a
HuWeilin <kevislin@HuWeilindeMacBook-Air.local>
2022-03-21 20:11:57 +0800

    修改wandb数据上报

--- main.py
+++ main.py
@@ -258,7 +258,19 @@ config = {
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 wandb.init(project="Cell_Classification", entity="kevislin")
-wandb.config = config
+wandb.config = {
+    'epoch_GCN': 10000,  # Huang model 训练的epoch
+    'epoch_CPM_train': 4000,
+    'epoch_CPM_test': 4000,
+    'lsd_dim': 128,  # CPM_net latent space dimension
+    'GNN_lr': 0.001,
+    'CPM_lr': [0.001, 0.001],  # CPM_ner中train和test的学习率
+    'ref_class_num': 9,  # Reference data的类别数
+    'query_class_num': 9,  # query data的类别数
+    'k': 4,  # 图构造的时候k_neighbor参数
+    'middle_out': 256,  # GCN中间层维数
+    'w_classify': 5  # classfication loss的权重
+}
 
 ret = transfer_label(dataPath, labelPath, SMPath, config)
 
