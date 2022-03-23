92226189abde20e944845cddc77f65ebf3494715
HuWeilin <kevislin@HuWeilindeMacBook-Air.local>
2022-03-24 01:14:22 +0800

    修改R文件

--- clustering_by_integreting_pathway.R
+++ clustering_by_integreting_pathway.R
@@ -4,7 +4,15 @@
 # BiocManager::install("SingleCellExperiment")
 if (!require("BiocManager", quietly = TRUE))
     install.packages("BiocManager")
+
+BiocManager::install("annotate")
+BiocManager::install("AnnotationDbi")
+BiocManager::install("XML")
 BiocManager::install("GSEABase")
+
+
+
+
 # BiocManager::install("GSEABase")
 # install.packages("AUCell")
 # install.packages("SingleCellExperiment",repos="https://mirrors.tuna.tsinghua.edu.cn/CRAN/")
