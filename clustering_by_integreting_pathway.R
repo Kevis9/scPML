df27cd1dbc459cf9740ff87b1dd3bcf04129f54a
HuWeilin <kevislin@HuWeilindeMacBook-Air.local>
2022-03-25 13:07:14 +0800

    修改R

--- clustering_by_integreting_pathway.R
+++ clustering_by_integreting_pathway.R
@@ -269,15 +269,15 @@ main<-function(paName, scName,s, paPath, save_path){
 
 
 scName= 'yan'
-paPath = "/home/yuanhuang/kevislin/data/pathway"
-mat_path = '/home/yuanhuang/kevislin/data/transfer_across_species_data/'
+paPath = "./yuanhuang/kevislin/data/pathway"
+mat_path = './yuanhuang/kevislin/data/transfer_across_species_data/'
 
 mat_name = 'human_pancreas.csv'
 mat_gene = load_matrix_for_GSE(paste(mat_path, mat_name, sep=''))
 mat_gene = t(mat_gene) # 对于(cell*genes)格式的数据，先做一次转置
 save_path = paste(mat_path, 'similarity_mat/SM_human_pancreas_', sep='')
 
-main('KEGG', scName,'human', paPath, save_path)
+# main('KEGG', scName,'human', paPath, save_path)
 main('Reactome', scName,'human', paPath, save_path)
 main('Wikipathways', scName,'human', paPath, save_path)
 main('de novo pathway', scName,'human', paPath, save_path)
