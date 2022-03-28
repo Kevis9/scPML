f5cb4cd14f0dc055a35a1ac8c6af67a51ff1a0b4
HuWeilin <kevislin@HuWeilindeMacBook-Air.local>
2022-03-29 00:02:03 +0800

    species测试

--- main.py
+++ main.py
@@ -216,8 +216,8 @@ SMPath = {
 }
 
 config = {
-    'epoch_GCN': 4000,  # Huang model 训练的epoch
-    'epoch_CPM_train': 4000,
+    'epoch_GCN': 5000,  # Huang model 训练的epoch
+    'epoch_CPM_train': 1600,
     'epoch_CPM_test': 4000,
     'lsd_dim': 128,  # CPM_net latent space dimension
     'GNN_lr': 0.001,
@@ -226,7 +226,7 @@ config = {
     'query_class_num': 9,  # query data的类别数
     'k': 4,  # 图构造的时候k_neighbor参数
     'middle_out': 256,  # GCN中间层维数
-    'w_classify': 5  # classfication loss的权重
+    'w_classify': 10  # classfication loss的权重
 }
 
 
