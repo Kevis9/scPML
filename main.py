5cbb332b722cff4d33e84d44880a98a2344f5e21
HuWeilin <kevislin@HuWeilindeMacBook-Air.local>
2022-03-30 12:31:26 +0800

    platform cel_seq_10x_v3: k = 2

--- main.py
+++ main.py
@@ -254,7 +254,7 @@ SMPath = {
 config = {
     'epoch_GCN': 6000,  # Huang model 训练的epoch
     'epoch_CPM_train': 1000,
-    'epoch_CPM_test': 8000,
+    'epoch_CPM_test': 5000,
     'lsd_dim': 128,  # CPM_net latent space dimension
     'GNN_lr': 0.001,
     'CPM_lr': [0.001, 0.001],  # CPM_ner中train和test的学习率
@@ -262,7 +262,7 @@ config = {
     'query_class_num': 7,  # query data的类别数
     'k':2,  # 图构造的时候k_neighbor参数
     'middle_out': 256,  # GCN中间层维数
-    'w_classify': 5,  # classfication loss的权重
+    'w_classify': 50,  # classfication loss的权重
 }
 
 
