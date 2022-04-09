783dff9edd64309c2fcea6337d466e942ad2467f
HuWeilin <kevislin@HuWeilindeMacBook-Air.local>
2022-04-09 11:37:15 +0800

    超参数调整

--- main.py
+++ main.py
@@ -234,8 +234,8 @@ SMPath = {
 config = {
     'epoch_GCN': 2500,  # Huang model 训练的epoch
     'epoch_CPM_train': 3000,
-    'epoch_CPM_test': 4000,
-    'lsd_dim': 64,  # CPM_net latent space dimension
+    'epoch_CPM_test': 2000,
+    'lsd_dim': 128,  # CPM_net latent space dimension
     'GNN_lr': 0.001,
     'CPM_lr': [0.001, 0.001, 0.01],  # CPM_ner中net和train_h,test_h的学习率
     'ref_class_num': 8,  # Reference data的类别数
