2b8384bd673aa0a8dba56e10903c9cae36b2e870
HuWeilin <kevislin@HuWeilindeMacBook-Air.local>
2022-04-10 21:22:12 +0800

    species

--- main.py
+++ main.py
@@ -194,8 +194,8 @@ def transfer_label(data_path: dict,
 # 数据配置
 data_config = {
     'data_path': '/home/zhianhuang/yuanhuang/kevislin/data/species_data/GSE84133',
-    'ref_name': 'mouse',
-    'query_name': 'human',
+    'ref_name': 'human',
+    'query_name': 'mouse',
     'project': 'species'
 }
 
@@ -227,17 +227,17 @@ SMPath = {
 }
 
 config = {
-    'epoch_GCN': 30000,  # Huang model 训练的epoch
-    'epoch_CPM_train': 30000,
-    'epoch_CPM_test': 30000,
-    'lsd_dim': 128,  # CPM_net latent space dimension
+    'epoch_GCN': 3000,  # Huang model 训练的epoch
+    'epoch_CPM_train': 3000,
+    'epoch_CPM_test': 3000,
+    'lsd_dim': 512,  # CPM_net latent space dimension
     'GNN_lr': 0.001,
     'CPM_lr': [0.001, 0.001, 0.01],  # CPM_ner中net和train_h,test_h的学习率
     'ref_class_num': 8,  # Reference data的类别数
     'query_class_num': 8,  # query data的类别数
     'k': 2,  # 图构造的时候k_neighbor参数
     'middle_out': 1500,  # GCN中间层维数
-    'w_classify': 0.2,  # classfication loss的权重
+    'w_classify': 10,  # classfication loss的权重
 }
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
