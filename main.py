39775ea2b2e9f8360a227e18decde94888f0f66b
HuWeilin <kevislin@HuWeilindeMacBook-Air.local>
2022-04-10 14:01:00 +0800

    cel_seq_indrop

--- main.py
+++ main.py
@@ -236,8 +236,8 @@ config = {
     'ref_class_num': 7,  # Reference data的类别数
     'query_class_num': 7,  # query data的类别数
     'k': 2,  # 图构造的时候k_neighbor参数
-    'middle_out': 8000,  # GCN中间层维数
-    'w_classify': 1,  # classfication loss的权重
+    'middle_out': 2000,  # GCN中间层维数
+    'w_classify': 0.1,  # classfication loss的权重
 }
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
