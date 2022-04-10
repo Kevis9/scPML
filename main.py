7677855df057045d1ded6a9664f22f495e4949d5
HuWeilin <kevislin@HuWeilindeMacBook-Air.local>
2022-04-10 15:46:21 +0800

    cel_seq_indrop

--- main.py
+++ main.py
@@ -235,8 +235,8 @@ config = {
     'CPM_lr': [0.001, 0.001, 0.01],  # CPM_ner中net和train_h,test_h的学习率
     'ref_class_num': 7,  # Reference data的类别数
     'query_class_num': 7,  # query data的类别数
-    'k': 2,  # 图构造的时候k_neighbor参数
-    'middle_out': 256,  # GCN中间层维数
+    'k': 1,  # 图构造的时候k_neighbor参数
+    'middle_out': 6000,  # GCN中间层维数
     'w_classify': 1,  # classfication loss的权重
 }
 
