ca8da670bbbcdc188d5636005d768118380ee35e
HuWeilin <kevislin@HuWeilindeMacBook-Air.local>
2022-04-12 01:37:37 +0800

    这是auto_git, 一般是调参

--- main.py
+++ main.py
@@ -226,7 +226,7 @@ config = {
     'query_class_num': data_config['class_num'],  # query data的类别数
     'k': 2,  # 图构造的时候k_neighbor参数
     'middle_out': 1500,  # GCN中间层维数
-    'w_classify': 0.2,  # classfication loss的权重
+    'w_classify': 1,  # classfication loss的权重
 }
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
