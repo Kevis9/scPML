b5e307a8372d6657fe598aa425735fad7e8283a5
HuWeilin <kevislin@HuWeilindeAir.lan>
2022-04-17 00:40:34 +0800

    这是auto_git, 一般是调参

--- main.py
+++ main.py
@@ -203,7 +203,7 @@ data_config = {
 config = {
     'epoch_GCN': 3000,  # Huang model 训练的epoch
     'epoch_CPM_train': 3000,
-    'epoch_CPM_test': 4000,
+    'epoch_CPM_test': 3000,
     'lsd_dim': 128,  # CPM_net latent space dimension
     'GNN_lr': 0.001,
     'CPM_lr': [0.001, 0.001, 0.001],  # CPM_ner中net和train_h,test_h的学习率
@@ -211,7 +211,7 @@ config = {
     'query_class_num': data_config['class_num'],  # query data的类别数
     'k': 2,  # 图构造的时候k_neighbor参数
     'middle_out': 3000,  # GCN中间层维数
-    'w_classify': 100,  # classfication loss的权重
+    'w_classify': 1,  # classfication loss的权重
     'note':''
 }
 
