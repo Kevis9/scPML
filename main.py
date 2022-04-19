0c3c21df8ce919e4ffadd701f869638013d90fa1
HuWeilin <kevislin@HuWeilindeMacBook-Air.local>
2022-04-19 14:41:06 +0800

    这是auto_git, 一般是调参

--- main.py
+++ main.py
@@ -198,7 +198,7 @@ config = {
     'ref_class_num': data_config['class_num'],  # Reference data的类别数
     'query_class_num': data_config['class_num'],  # query data的类别数
     'k': 2,  # 图构造的时候k_neighbor参数
-    'middle_out': 256,  # GCN中间层维数
+    'middle_out': 512,  # GCN中间层维数
     'w_classify': 100,  # classfication loss的权重
     'note':"human use human's pathway, mouse use mouse's"
 }
