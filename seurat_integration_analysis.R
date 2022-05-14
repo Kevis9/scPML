70472fca472ed37a5960360725ab736847d6a864
HuWeilin <kevislin@HuWeilindeMacBook-Air.local>
2022-05-14 22:08:22 +0800

    这是auto_git, 一般是调参

--- seurat_integration_analysis.R
+++ seurat_integration_analysis.R
@@ -11,8 +11,9 @@ atac_chr = read.csv('atac_rna_to_R/atac_chr.csv')
 atac_cell = read.csv('atac_rna_to_R/atac_cell.csv')
 
 print(dim(atac_data))
+
+print(dim(atac_chr['peak']))
 print("xxx")
-# print(dim(atac_chr['peak']))
 print(dim(atac_cell['sample']))
 
 
