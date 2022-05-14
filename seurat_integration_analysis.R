bcf8195b3c36fbdecce6fd6d02dc6e5a4a0b9747
HuWeilin <kevislin@HuWeilindeMacBook-Air.local>
2022-05-14 21:16:54 +0800

    这是auto_git, 一般是调参

--- seurat_integration_analysis.R
+++ seurat_integration_analysis.R
@@ -2,5 +2,12 @@ library(Seurat)
 library(ggplot2)
 library(patchwork)
 
-peaks = Matrix::readMM('/home/zhianhuang/yuanhuang/kevislin/data/raw_data/omics_data/A549/ATAC/GSM3271041_ATAC_sciCAR_A549_peak_count.txt')
-print(as.matrix(peaks))
+atac_data = Matrix::readMM('/home/zhianhuang/yuanhuang/kevislin/data/raw_data/omics_data/A549/ATAC/GSM3271041_ATAC_sciCAR_A549_peak_count.txt')
+atac_data = as.matrix(atac_data)
+
+atac_chr = read.csv('/home/zhianhuang/yuanhuang/kevislin/data/raw_data/omics_data/A549/ATAC/GSM3271041_ATAC_sciCAR_A549_peak.txt')
+atac_cell = read.csv('/home/zhianhuang/yuanhuang/kevislin/data/raw_data/omics_data/A549/ATAC/GSM3271041_ATAC_sciCAR_A549_cell.txt')
+
+print(atac_chr['chr'])
+
+# activity.matrix =
