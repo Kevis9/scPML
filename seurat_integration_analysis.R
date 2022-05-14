9b216fa220e8c0a06ad891f3efc0894b55a4c4a1
HuWeilin <kevislin@HuWeilindeMacBook-Air.local>
2022-05-14 22:02:13 +0800

    这是auto_git, 一般是调参

--- seurat_integration_analysis.R
+++ seurat_integration_analysis.R
@@ -2,12 +2,66 @@ library(Seurat)
 library(ggplot2)
 library(patchwork)
 
-atac_data = Matrix::readMM('/home/zhianhuang/yuanhuang/kevislin/data/raw_data/omics_data/A549/ATAC/GSM3271041_ATAC_sciCAR_A549_peak_count.txt')
-atac_data = as.matrix(atac_data)
+# atac_data
+atac_data = Matrix::readMM('atac_rna_to_R/atac_data.txt')
+atac_data = t(as.matrix(atac_data))
 
-atac_chr = read.csv('/home/zhianhuang/yuanhuang/kevislin/data/raw_data/omics_data/A549/ATAC/GSM3271041_ATAC_sciCAR_A549_peak.txt')
-atac_cell = read.csv('/home/zhianhuang/yuanhuang/kevislin/data/raw_data/omics_data/A549/ATAC/GSM3271041_ATAC_sciCAR_A549_cell.txt')
+atac_chr = read.csv('atac_rna_to_R/atac_chr.csv')
+atac_cell = read.csv('atac_rna_to_R/atac_cell.csv')
 
-rownames(atac_data) = list(atac_cell['sample'])
+rownames(atac_data) = list(atac_chr['peak'])
+colnames(atac_data) = list(atac_cell['sample'])
+
+# rna_data
+rna_data = Matrix::readMM('atac_rna_to_R/rna_data.txt')
+rna_data = t(as.matrix(rna_data))
+
+rna_gene = read.csv('atac_rna_to_R/rna_gene.csv')
+rna_cell = read.csv('atac_rna_to_R/rna_cell.csv')
+
+rownames(rna_data) = list(rna_gene['gene_name'])
+colnames(rna_data) = list(rna_cell['sample'])
+
+# 重头戏
+
+activity.matrix <- CreateGeneActivityMatrix(peak.matrix = atac_data, annotation.file = "Homo_sapiens.GRCh37.82.gtf",
+    seq.levels = c(1:22, "X", "Y"), upstream = 2000, verbose = TRUE)
+
+pbmc.atac <- CreateSeuratObject(counts = peaks, assay = "ATAC", project = "10x_ATAC")
+pbmc.atac[["ACTIVITY"]] <- CreateAssayObject(counts = activity.matrix)
+pbmc.atac$tech <- "atac"
+
+
+DefaultAssay(pbmc.atac) <- "ACTIVITY"
+pbmc.atac <- FindVariableFeatures(pbmc.atac)
+pbmc.atac <- NormalizeData(pbmc.atac)
+pbmc.atac <- ScaleData(pbmc.atac)
+
+pbmc.rna <- rna_data
+pbmc.rna$tech <- "rna"
+
+
+genes.use <- VariableFeatures(pbmc.rna)
+refdata <- GetAssayData(pbmc.rna, assay = "RNA", slot = "data")[genes.use, ]
+
+# refdata (input) contains a scRNA-seq expression matrix for the scRNA-seq cells.  imputation
+# (output) will contain an imputed scRNA-seq matrix for each of the ATAC cells
+imputation <- TransferData(anchorset = transfer.anchors, refdata = refdata, weight.reduction = pbmc.atac[["lsi"]])
+
+# this line adds the imputed data matrix to the pbmc.atac object
+pbmc.atac[["RNA"]] <- imputation
+coembed <- merge(x = pbmc.rna, y = pbmc.atac)
+
+# Finally, we run PCA and UMAP on this combined object, to visualize the co-embedding of both
+# datasets
+coembed <- ScaleData(coembed, features = genes.use, do.scale = FALSE)
+coembed <- RunPCA(coembed, features = genes.use, verbose = FALSE)
+coembed <- RunUMAP(coembed, dims = 1:30)
+
+
+p1 <- DimPlot(coembed, group.by = "tech")
+
+ggsave('atac_rna_to_R/atac_rna.png', p1)
+
 
 # activity.matrix =
