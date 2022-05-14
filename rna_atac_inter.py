affe36be69d4b247b99f2564985aefcfadd51ee1
HuWeilin <kevislin@HuWeilindeMacBook-Air.local>
2022-05-14 19:01:38 +0800

    这是auto_git, 一般是调参

--- rna_atac_inter.py
+++ rna_atac_inter.py
@@ -30,21 +30,17 @@ rna_df.columns = rna_gene['gene_short_name'].tolist()
 atac_activity_df = pd.read_csv('atac_activity_mat.csv', index_col=0).T
 commom_gene = list(set(atac_activity_df.columns.tolist()) & set(rna_df.columns.tolist()))
 
-print(len(commom_gene))
-print(commom_gene)
 
 
 atac_cell_name = atac_activity_df.index.str.replace(".", "-", 3).tolist()
 atac_activity_df.index = atac_cell_name
-
 common_cell = list(set(atac_cell_name) & set(rna_df.index.tolist()))
 
 
 atac_df = atac_activity_df.loc[common_cell, commom_gene]
 rna_df = rna_df.loc[common_cell, commom_gene]
-print(rna_df.shape)
-print(atac_df.shape)
 
+print(rna_df.columns)
 
 # 最后获取label
 label_idx = (rna_cell['sample'].isin(rna_df.index.tolist())).tolist()
