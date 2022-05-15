ac3cc939243bd92d4b59f9dfd0e674f6d21c242d
HuWeilin <kevislin@HuWeilindeMacBook-Air.local>
2022-05-15 13:51:54 +0800

    这是auto_git, 一般是调参

--- utils.py
+++ utils.py
@@ -182,7 +182,7 @@ def show_cluster(data, label, title):
     df = pd.DataFrame(data=data)
     plt.figure(figsize=(8, 6))
 
-    sns.scatterplot(data=df, x='x', y='y', hue='label', palette='deep', s=8)
+    sns.scatterplot(data=df, x='x', y='y', hue='label', palette='deep', s=6)
     plt.legend(loc=3, bbox_to_anchor=(1, 0)) # 设置图例位置
     plt.xlabel('UMAP1')
     plt.ylabel('UMAP2')
