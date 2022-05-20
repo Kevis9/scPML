07c30f77a39f4cff4fc7844fe7fbd67bce3fa828
HuWeilin <kevislin@HuWeilindeMacBook-Air.local>
2022-05-20 15:34:07 +0800

    这是auto_git, 一般是调参

--- main.py
+++ main.py
@@ -319,7 +319,7 @@ def transfer_label(data_path: dict,
 
 # 数据配置
 data_config = {
-    'data_path': '/home/zhianhuang/yuanhuang/kevislin/data/omics_data/PBMC/processed_data',
+    'data_path': '/home/zhianhuang/yuanhuang/kevislin/data/omics_data/A549',
     'ref_name': 'rna',
     'query_name': 'atac',
     'project': 'omics',
