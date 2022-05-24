e400773e89a4b3c62294c3ce4b1670a144840742
HuWeilin <kevislin@HuWeilindeMacBook-Air.local>
2022-05-24 10:49:22 +0800

    这是auto_git, 一般是调参

--- peak_annotation.py
+++ peak_annotation.py
@@ -11,7 +11,6 @@ import pandas as pd
 atac_data = spio.mmread('/home/zhianhuang/yuanhuang/kevislin/data/raw_data/omics_data/A549/ATAC/GSM3271041_ATAC_sciCAR_A549_peak_count.txt')
 atac_data = atac_data.todense().T
 
-
 atac_cell = pd.read_csv('/home/zhianhuang/yuanhuang/kevislin/data/raw_data/omics_data/A549/ATAC/GSM3271041_ATAC_sciCAR_A549_cell.txt')
 atac_chr = pd.read_csv('/home/zhianhuang/yuanhuang/kevislin/data/raw_data/omics_data/A549/ATAC/GSM3271041_ATAC_sciCAR_A549_peak.txt', dtype={'chr':object})
 
@@ -24,20 +23,21 @@ atac_cell = atac_cell.iloc[cell_idx, :]
 atac_df = atac_df.iloc[cell_idx, :]
 
 # 保留1~22, X, Y染色体
-chr_arr = [str(x) for x in range(1, 23)]
+# chr_arr = [str(x) for x in range(1, 23)]
+#
+# chr_arr += ['X', 'Y']
+# chr_idx = atac_chr['chr'].isin(chr_arr).tolist()
+#
+#
+# atac_chr = atac_chr.iloc[chr_idx, :]
+#
+# atac_df = atac_df.iloc[:, chr_idx]
 
-chr_arr += ['X', 'Y']
-chr_idx = atac_chr['chr'].isin(chr_arr).tolist()
-
-
-atac_chr = atac_chr.iloc[chr_idx, :]
-
-atac_df = atac_df.iloc[:, chr_idx]
 
 # 设置index和columns, 注意columns的格式
 atac_df.index = atac_cell['sample'].tolist()
-atac_df.columns = (atac_chr['peak'].map(lambda x: ('chr' + x).replace('-', ':', 1))).tolist()
-
+# atac_df.columns = (atac_chr['peak'].map(lambda x: ('chr' + x).replace('-', ':', 1))).tolist()
+atac_df.columns = (atac_chr['peak'].map(lambda x: (x).replace('-', ':', 1))).tolist()
 
 # 保存 gene*cell
 (atac_df.T).to_csv('atac_middle_out.csv')
