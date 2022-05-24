abc36cc42b3bee3f5d654b3b09fc0e6cacd2c36d
HuWeilin <kevislin@HuWeilindeMacBook-Air.local>
2022-05-24 13:16:18 +0800

    这是auto_git, 一般是调参

--- peak_annotation.R
+++ peak_annotation.R
@@ -44,7 +44,7 @@ library(patchwork)
 #                 'KE332505.1', 'KE332506.1', 'M', 'chrX', 'chrY', 'hs37d5')
 
 chromosomes = c('chr1', 'chr10', 'chr11', 'chr12', 'chr13', 'chr14', 'chr15', 'chr16', 'chr17', 'chr18', 'chr19',
-                'chr2', 'chr20', 'chr21', 'chr22', 'chr3', 'chr4', 'chr5', 'chr5', 'chr6', 'chr7', 'chr8', 'chr9', 'chrX', 'chrY')
+                'chr2', 'chr20', 'chr21', 'chr22', 'chr3', 'chr4', 'chr5', 'chr6', 'chr7', 'chr8', 'chr9', 'chrX', 'chrY')
 
 atac_data = read.csv('atac_middle_out.csv', row.names=1)
 # print(rownames(atac_data))
