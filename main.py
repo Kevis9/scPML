d23c025dd3d18048f3bf7c5b90150bfbf8198f28
HuWeilin <kevislin@HuWeilindeMacBook-Air.local>
2022-06-01 21:56:09 +0800

    这是auto_git, 一般是调参

--- main.py
+++ main.py
@@ -212,7 +212,7 @@ config = {
     'query_class_num': data_config['class_num'],  # query data的类别数
     'k': 2,  # 图构造的时候k_neighbor参数
     'middle_out': 6000,  # GCN中间层维数
-    'w_classify': 1,  # classfication loss的权重
+    'w_classify': 100,  # classfication loss的权重
     'cen_weight': 0.5,
 }
 
