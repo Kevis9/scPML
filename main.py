3525d3e829c8c1b321055d1681e0d834f2dfc52e
HuWeilin <kevislin@HuWeilindeMacBook-Air.local>
2022-06-02 23:26:38 +0800

    这是auto_git, 一般是调参

--- main.py
+++ main.py
@@ -232,9 +232,9 @@ data_config = {
 #{'gamma', 'alpha', 'endothelial', 'macrophage', 'ductal', 'delta', 'beta', 'quiescent_stellate'}
 
 config = {
-    'epoch_GCN': 800,  # Huang model 训练的epoch
-    'epoch_CPM_train': 1500,
-    'epoch_CPM_test': 1500,
+    'epoch_GCN': 2500,  # Huang model 训练的epoch
+    'epoch_CPM_train': 3000,
+    'epoch_CPM_test': 3000,
     'lsd_dim': 128,  # CPM_net latent space dimension
     'GNN_lr': 0.001,
     'CPM_lr': [0.001, 0.001, 0.001],  # CPM_ner中net和train_h,test_h的学习率
@@ -242,7 +242,7 @@ config = {
     'query_class_num': data_config['class_num'],  # query data的类别数
     'k': 2,  # 图构造的时候k_neighbor参数
     'middle_out': 1500,  # GCN中间层维数
-    'w_classify': 10,  # classfication loss的权重
+    'w_classify': 1,  # classfication loss的权重
     'cen_weight': 0.5,
 }
 
