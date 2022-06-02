ed13c6fce59019ce72bfd9e4780408eafe7880bf
HuWeilin <kevislin@HuWeilindeMacBook-Air.local>
2022-06-02 14:25:27 +0800

    这是auto_git, 一般是调参

--- main.py
+++ main.py
@@ -193,8 +193,8 @@ def transfer_train(data_config: dict,
 # 数据配置
 data_config = {
     'data_path': '/home/zhianhuang/yuanhuang/kevislin/data/species_data/GSE84133/no_diff',
-    'ref_name': 'human',
-    'query_name': 'mouse',
+    'ref_name': 'mouse',
+    'query_name': 'human',
     'project': 'species',
     'class_num': 8,
     'dataset_name':'GSE84133'
@@ -202,16 +202,16 @@ data_config = {
 #{'gamma', 'alpha', 'endothelial', 'macrophage', 'ductal', 'delta', 'beta', 'quiescent_stellate'}
 
 config = {
-    'epoch_GCN': 600,  # Huang model 训练的epoch
-    'epoch_CPM_train': 3000,
-    'epoch_CPM_test': 3000,
+    'epoch_GCN': 500,  # Huang model 训练的epoch
+    'epoch_CPM_train': 1200,
+    'epoch_CPM_test': 1200,
     'lsd_dim': 128,  # CPM_net latent space dimension
     'GNN_lr': 0.001,
     'CPM_lr': [0.001, 0.001, 0.001],  # CPM_ner中net和train_h,test_h的学习率
     'ref_class_num': data_config['class_num'],  # Reference data的类别数
     'query_class_num': data_config['class_num'],  # query data的类别数
     'k': 2,  # 图构造的时候k_neighbor参数
-    'middle_out': 9000,  # GCN中间层维数
+    'middle_out': 8000,  # GCN中间层维数
     'w_classify': 100,  # classfication loss的权重
     'cen_weight': 0.5,
 }
