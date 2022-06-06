2bd50499b475263c63647f25628b0cb6c73a6d21
HuWeilin <kevislin@HuWeilindeMacBook-Air.local>
2022-06-07 00:16:35 +0800

    这是auto_git, 一般是调参

--- platform_pbmc_construct.py
+++ platform_pbmc_construct.py
@@ -29,75 +29,93 @@ data6.columns = gene
 
 
 
-def save_data_label(data, label, c_type, path):
+
+
+
+
+
+def save_data_label(data, label, c_type, path1, path2):
     idx = label['CellType'].isin(c_type).tolist()
     new_data = data.iloc[idx, :]
     new_label = label.iloc[idx, :]
-    new_data.to_csv(path+"\\data.csv")
-    new_label.to_csv(path+"\\label.csv", index=False)
+    new_data.to_csv(path1)
+    new_label.to_csv(path2, index=False)
+    # new_data.to_csv(path+"\\data.csv")
+    # new_label.to_csv(path+"\\label.csv", index=False)
 
+
+common_type = set(label1['CellType'].tolist()) & set(label2['CellType'].tolist()) & \
+              set(label3['CellType'].tolist()) & set(label4['CellType'].tolist()) & \
+              set(label5['CellType'].tolist()) & set(label6['CellType'].tolist())
+
+save_data_label(data1, label1, "data1.csv", "label1.csv")
+save_data_label(data2, label2, "data2.csv", "label2.csv")
+save_data_label(data3, label3, "data3.csv", "label3.csv")
+save_data_label(data4, label4, "data4.csv", "label4.csv")
+save_data_label(data5, label5, "data5.csv", "label5.csv")
+save_data_label(data6, label6, "data6.csv", "label6.csv")
 
 # task1: ref: data1
-common_type = list(set(label1['CellType'].tolist()))
-save_data_label(data2, label2, common_type, "task1\\query\\query_1")
-save_data_label(data3, label3, common_type, "task1\\query\\query_2")
-save_data_label(data4, label4, common_type, "task1\\query\\query_3")
-save_data_label(data5, label5, common_type, "task1\\query\\query_4")
-save_data_label(data6, label6, common_type, "task1\\query\\query_5")
-data1.to_csv("task1\\ref\\data.csv")
-label1.to_csv("task1\\ref\\label.csv")
-
-
-# task2: ref: data2
-common_type = list(set(label2['CellType'].tolist()))
-save_data_label(data1, label1, common_type, "task2\\query\\query_1")
-save_data_label(data3, label3, common_type, "task2\\query\\query_2")
-save_data_label(data4, label4, common_type, "task2\\query\\query_3")
-save_data_label(data5, label5, common_type, "task2\\query\\query_4")
-save_data_label(data6, label6, common_type, "task2\\query\\query_5")
-data2.to_csv("task2\\ref\\data.csv")
-label2.to_csv("task2\\ref\\label.csv")
-
-# task3 : ref: data3
-common_type = list(set(label3['CellType'].tolist()))
-save_data_label(data1, label1, common_type, "task3\\query\\query_1")
-save_data_label(data2, label2, common_type, "task3\\query\\query_2")
-save_data_label(data4, label4, common_type, "task3\\query\\query_3")
-save_data_label(data5, label5, common_type, "task3\\query\\query_4")
-save_data_label(data6, label6, common_type, "task3\\query\\query_5")
-data3.to_csv("task3\\ref\\data.csv")
-label3.to_csv("task3\\ref\\label.csv")
-
-
-# task4 : ref: data4
-common_type = list(set(label4['CellType'].tolist()))
-save_data_label(data1, label1, common_type, "task4\\query\\query_1")
-save_data_label(data2, label2, common_type, "task4\\query\\query_2")
-save_data_label(data3, label3, common_type, "task4\\query\\query_3")
-save_data_label(data5, label5, common_type, "task4\\query\\query_4")
-save_data_label(data6, label6, common_type, "task4\\query\\query_5")
-data4.to_csv("task4\\ref\\data.csv")
-label4.to_csv("task4\\ref\\label.csv")
-
-# task5 : ref: data5
-common_type = list(set(label5['CellType'].tolist()))
-save_data_label(data1, label1, common_type, "task5\\query\\query_1")
-save_data_label(data2, label2, common_type, "task5\\query\\query_2")
-save_data_label(data3, label3, common_type, "task5\\query\\query_3")
-save_data_label(data4, label4, common_type, "task5\\query\\query_4")
-save_data_label(data6, label6, common_type, "task5\\query\\query_5")
-data5.to_csv("task5\\ref\\data.csv")
-label5.to_csv("task5\\ref\\label.csv")
-
-# task6 : ref: data6
-common_type = list(set(label6['CellType'].tolist()))
-save_data_label(data1, label1, common_type, "task6\\query\\query_1")
-save_data_label(data2, label2, common_type, "task6\\query\\query_2")
-save_data_label(data3, label3, common_type, "task6\\query\\query_3")
-save_data_label(data4, label4, common_type, "task6\\query\\query_4")
-save_data_label(data5, label5, common_type, "task6\\query\\query_5")
-data6.to_csv("task6\\ref\\data.csv")
-label6.to_csv("task6\\ref\\label.csv")
+# common_type = list(set(label1['CellType'].tolist()))
+# save_data_label(data2, label2, common_type, "task1\\query\\query_1")
+# save_data_label(data3, label3, common_type, "task1\\query\\query_2")
+# save_data_label(data4, label4, common_type, "task1\\query\\query_3")
+# save_data_label(data5, label5, common_type, "task1\\query\\query_4")
+# save_data_label(data6, label6, common_type, "task1\\query\\query_5")
+# data1.to_csv("task1\\ref\\data.csv")
+# label1.to_csv("task1\\ref\\label.csv")
+#
+#
+# # task2: ref: data2
+# common_type = list(set(label2['CellType'].tolist()))
+# save_data_label(data1, label1, common_type, "task2\\query\\query_1")
+# save_data_label(data3, label3, common_type, "task2\\query\\query_2")
+# save_data_label(data4, label4, common_type, "task2\\query\\query_3")
+# save_data_label(data5, label5, common_type, "task2\\query\\query_4")
+# save_data_label(data6, label6, common_type, "task2\\query\\query_5")
+# data2.to_csv("task2\\ref\\data.csv")
+# label2.to_csv("task2\\ref\\label.csv")
+#
+# # task3 : ref: data3
+# common_type = list(set(label3['CellType'].tolist()))
+# save_data_label(data1, label1, common_type, "task3\\query\\query_1")
+# save_data_label(data2, label2, common_type, "task3\\query\\query_2")
+# save_data_label(data4, label4, common_type, "task3\\query\\query_3")
+# save_data_label(data5, label5, common_type, "task3\\query\\query_4")
+# save_data_label(data6, label6, common_type, "task3\\query\\query_5")
+# data3.to_csv("task3\\ref\\data.csv")
+# label3.to_csv("task3\\ref\\label.csv")
+#
+#
+# # task4 : ref: data4
+# common_type = list(set(label4['CellType'].tolist()))
+# save_data_label(data1, label1, common_type, "task4\\query\\query_1")
+# save_data_label(data2, label2, common_type, "task4\\query\\query_2")
+# save_data_label(data3, label3, common_type, "task4\\query\\query_3")
+# save_data_label(data5, label5, common_type, "task4\\query\\query_4")
+# save_data_label(data6, label6, common_type, "task4\\query\\query_5")
+# data4.to_csv("task4\\ref\\data.csv")
+# label4.to_csv("task4\\ref\\label.csv")
+#
+# # task5 : ref: data5
+# common_type = list(set(label5['CellType'].tolist()))
+# save_data_label(data1, label1, common_type, "task5\\query\\query_1")
+# save_data_label(data2, label2, common_type, "task5\\query\\query_2")
+# save_data_label(data3, label3, common_type, "task5\\query\\query_3")
+# save_data_label(data4, label4, common_type, "task5\\query\\query_4")
+# save_data_label(data6, label6, common_type, "task5\\query\\query_5")
+# data5.to_csv("task5\\ref\\data.csv")
+# label5.to_csv("task5\\ref\\label.csv")
+#
+# # task6 : ref: data6
+# common_type = list(set(label6['CellType'].tolist()))
+# save_data_label(data1, label1, common_type, "task6\\query\\query_1")
+# save_data_label(data2, label2, common_type, "task6\\query\\query_2")
+# save_data_label(data3, label3, common_type, "task6\\query\\query_3")
+# save_data_label(data4, label4, common_type, "task6\\query\\query_4")
+# save_data_label(data5, label5, common_type, "task6\\query\\query_5")
+# data6.to_csv("task6\\ref\\data.csv")
+# label6.to_csv("task6\\ref\\label.csv")
 
 
 
